30b12217905b6d1887d873fa83afe669c25fb6e5
Smiley98 <smileysofficialcinema@gmail.com>
2024-05-09 13:08:23 -0400

    Added cursor seeking

--- Assets/Scripts/LineSeek.cs
+++ Assets/Scripts/LineSeek.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class LineSeek : MonoBehaviour
 {
+    // Move at a speed of 5 units per second
+    float speed = 5.0f;
+
     void Start()
     {
         
     }
 
+    // Move the object towards the mouse cursor
     void Update()
     {
         Vector3 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mouse.z = 0.0f;
-        transform.position = mouse;
+        transform.position = Vector3.MoveTowards(transform.position, mouse, speed * Time.deltaTime);
     }
 }
