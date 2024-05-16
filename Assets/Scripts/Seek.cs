6a9bdc5afbd3a720b60246aa637b0f25c6becd0e
Smiley98 <smileysofficialcinema@gmail.com>
2024-05-16 13:16:09 -0400

    Added steering behaviour changing

--- Assets/Scripts/Seek.cs
+++ Assets/Scripts/Seek.cs
@@ -2,8 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Steering
+{
+    LINE,
+    SEEK
+}
+
 public class Seek : MonoBehaviour
 {
+    public Steering steering;
+
+
     float speed = 10.0f;
     Rigidbody2D rb;
 
@@ -14,23 +23,41 @@ public class Seek : MonoBehaviour
 
     void Update()
     {
-        // 1. Get the mouse position in world-space. This will be our seek target!
-        Vector3 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        mouse.z = 0.0f;
+        // Switch behaviour every time space is pressed
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (steering == Steering.SEEK)
+            {
+                steering = Steering.LINE;
 
-        // 2. Determine force vector (desired velocity - current velocity)
-        Vector3 currentVelocity = rb.velocity;
-        Vector3 desiredVelocity = (mouse - transform.position).normalized * speed;
-        Vector3 seekForce = desiredVelocity - currentVelocity;
+                // Must reset rigid-body velocity since line-seek is doesn't use rb velocity
+                rb.velocity = Vector3.zero;
+            }
+            else if (steering == Steering.LINE)
+            {
+                steering = Steering.SEEK;
+            }
+        }
 
-        // 3. Apply force vector
-        rb.AddForce(seekForce);
+        if (steering == Steering.LINE)
+        {
+            Vector3 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mouse.z = 0.0f;
+            transform.position = Vector3.MoveTowards(transform.position, mouse, speed * Time.deltaTime);
+        }
+        else if (steering == Steering.SEEK)
+        {
+            // 1. Get the mouse position in world-space. This will be our seek target!
+            Vector3 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mouse.z = 0.0f;
 
-        // Long-form calculation of desired velocity:
-        Vector3 A = transform.position; // let A = seeker position
-        Vector3 B = mouse;              // let B = target position
-        Vector3 AB = B - A;             // let AB = vector FROM seeker TO target
-        Vector3 unitAB = AB.normalized; // Normalize AB so space isn't distorted
-        Vector3 vAB = unitAB * speed;   // Multiply unitAB by speed to move in direction at desired speed!
+            // 2. Determine force vector (desired velocity - current velocity)
+            Vector3 currentVelocity = rb.velocity;
+            Vector3 desiredVelocity = (mouse - transform.position).normalized * speed;
+            Vector3 seekForce = desiredVelocity - currentVelocity;
+
+            // 3. Apply force vector
+            rb.AddForce(seekForce);
+        }
     }
 }
