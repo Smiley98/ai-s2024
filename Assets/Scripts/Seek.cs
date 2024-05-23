c60fb20a9351806904c8b2c0ec151e91eb0153a3
Smiley98 <smileysofficialcinema@gmail.com>
2024-05-23 13:31:41 -0400

    Finished steering state

--- Assets/Scripts/Seek.cs
+++ Assets/Scripts/Seek.cs
@@ -2,10 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Steering
+{
+    SEEK_LINE,
+    FLEE_LINE,
+    SEEK_CURVE,
+    FLEE_CURVE,
+    COUNT
+}
+
 public class Seek : MonoBehaviour
 {
     float speed = 10.0f;
     Rigidbody2D rb;
+    Steering behaviour = Steering.SEEK_LINE;
 
     void Start()
     {
@@ -35,7 +45,10 @@ public class Seek : MonoBehaviour
 
     Vector3 FleeCurve(Rigidbody2D seeker, Vector2 target, float speed)
     {
-        return -SeekCurve(seeker, target, speed);
+        //return -SeekCurve(seeker, target, speed);
+        Vector2 cv = seeker.velocity;
+        Vector2 dv = (seeker.position - target).normalized * speed;
+        return dv - cv;
     }
 
     void Update()
@@ -44,18 +57,52 @@ public class Seek : MonoBehaviour
         Vector3 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mouse.z = 0.0f;
 
-        transform.position += FleeLine(transform.position, mouse, speed);
+        if (behaviour == Steering.SEEK_LINE)
+        {
+            transform.position += SeekLine(transform.position, mouse, speed);
+        }
+        else if (behaviour == Steering.FLEE_LINE)
+        {
+            transform.position += FleeLine(transform.position, mouse, speed);
+        }
+        else if(behaviour == Steering.SEEK_CURVE)
+        {
+            rb.AddForce(SeekCurve(rb, mouse, speed));
+        }
+        else if(behaviour == Steering.FLEE_CURVE)
+        {
+            // How to limit a steering behaviour:
+            //Vector3 force = FleeCurve(rb, mouse, speed);
+            //Vector3.ClampMagnitude(force, speed * 0.1f);
+            rb.AddForce(FleeCurve(rb, mouse, speed));
+        }
 
-        // Seek target (position only, no rotation)
-        //Vector2 seekForce = SeekCurve(rb, mouse, speed);
-        //rb.AddForce(seekForce);
-
-        // Point at target
-        Vector3 direction = rb.velocity;
-        transform.right = direction;
+        // Cycle steering behaviours and ensure its within bounds
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            int b = (int)behaviour;
+            ++b;
+            b %= (int)Steering.COUNT;
+            behaviour = (Steering)b;
+        }
+        
+        // Behaviour-specific cases
+        if (behaviour == Steering.SEEK_LINE || behaviour == Steering.FLEE_LINE)
+        {
+            rb.velocity = Vector3.zero;
+            transform.right = Vector3.right;
+        }
+        else if (behaviour == Steering.SEEK_CURVE || behaviour == Steering.FLEE_CURVE)
+        {
+            Vector3 direction = rb.velocity;
+            transform.right = direction;
+        }
         // Homework task 2: add code to prevent the seeker from rotating if its 
         // approximately the same direction as the motion (prevent crazy jitter).
+
         // Reference Vector3.MoveTowards for the correction threshold.
+        // You only need to handle SEEK_CURVE's orientation for homework.
+        // The other 3 behaviours are just examples for how to switch states.
 
         // Render local axes
         float length = 10.0f;
