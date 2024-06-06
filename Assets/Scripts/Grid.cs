1283d109191d28b22ca769377bc018083ba76581
Smiley98 <smileysofficialcinema@gmail.com>
2024-06-06 13:19:27 -0400

    Added ColorTile function

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -7,27 +7,48 @@ public class Grid : MonoBehaviour
     [SerializeField]
     GameObject tilePrefab;
 
-    List<GameObject> tiles = new List<GameObject>();
+    int rows = 10;
+    int cols = 20;
+    List<List<GameObject>> tiles = new List<List<GameObject>>();
 
     void Start()
     {
-        int cols = 20;
         float xStart = 0.5f;
-        float yStart = 0.5f;
+        float yStart = rows - 0.5f;
         float x = xStart;
         float y = yStart;
-        for (int col = 0; col < cols; col++)
-        {
-            GameObject tile = Instantiate(tilePrefab);
-            tile.transform.position = new Vector3(x, y);
-            tiles.Add(tile);
 
-            x += 1.0f;
+        for (int row = 0; row < rows; row++)
+        {
+            // Create a new list of tiles for each row
+            tiles.Add(new List<GameObject>());
+
+            // Add a tile to the list for each column of the row
+            for (int col = 0; col < cols; col++)
+            {
+                GameObject tile = Instantiate(tilePrefab);
+                tile.transform.position = new Vector3(x, y);
+                tiles[row].Add(tile);
+
+                x += 1.0f;
+            }
+            x = xStart;
+            y -= 1.0f;
         }
+
+        
     }
 
     void Update()
     {
-        
+        ColorTile(tiles[0][0], Color.red);                  // top-left
+        ColorTile(tiles[0][cols - 1], Color.green);         // top-right
+        ColorTile(tiles[rows - 1][cols - 1], Color.blue);   // bot-right
+        ColorTile(tiles[rows - 1][0], Color.magenta);       // bot-left     
+    }
+    
+    void ColorTile(GameObject tile, Color color)
+    {
+        tile.GetComponent<SpriteRenderer>().color = color;
     }
 }
